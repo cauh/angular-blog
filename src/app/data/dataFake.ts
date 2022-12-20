84b7f8bfb19794b61f1c81bd19dd1ce2e8fd8e93
Claudia Hirose <claudiahirose@gmail.com>
2022-12-20 15:01:39 -0300

    Dynamic components

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Parque da Água Branca",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut est vel metus ultricies auctor. Praesent volutpat ornare nulla, mollis tempus justo luctus sit amet.",
+    "photoCover" : "assets/img/parqueAguaBranca.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Arquitetura linda",
+    "description": "Arquitetura linda",
+    "photoCover" : "assets/img/parqueAguaBranca2.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Bichos soltos pelo parque",
+    "description": "Bichos soltos pelo parque",
+    "photoCover" : "assets/img/pavao.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Feira de orgânicos",
+    "description": "Feira de orgânicos",
+    "photoCover" : "assets/img/parqueAguaBrancaFeiraOrganicos.jpg"
+  }
+]
+
